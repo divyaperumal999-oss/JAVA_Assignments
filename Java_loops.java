3ae3b760028f86930e2b586795e752bb2a44e381
divyaperumal999-oss <divyaperumal999@gmail.com>
2025-10-25 21:56:55 +0530

    Add files via upload

---
+++ Java_loops.java
@@ -0,0 +1,16 @@
+package Firstpackage;
+
+public class Java_loops {
+	public static void main(String args[])
+	{
+		for (int i=1;i<=100;i++)
+		{
+			if(i%5==0)
+			{
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+	}	
+	}
