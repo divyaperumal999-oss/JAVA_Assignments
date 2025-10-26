685610efe26596cb150b33cf41a57df6c8d621e7
divyaperumal999-oss <divyaperumal999@gmail.com>
2025-10-26 22:39:06 +0530

    Add files via upload

---
+++ Countdigits.java
@@ -0,0 +1,21 @@
+package Firstpackage;
+
+public class Countdigits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int inputnumber=12143;
+		int i=0;
+		int sum=0;
+ while(inputnumber!=0)
+ {
+	int dig=(inputnumber%10);// 
+	inputnumber=inputnumber/10;
+	sum=sum+dig;
+	i++;
+ }
+System.out.println("Count of digits:" +i); // 
+System.out.println("Sum of digits:" +sum );
+	}
+
+}
